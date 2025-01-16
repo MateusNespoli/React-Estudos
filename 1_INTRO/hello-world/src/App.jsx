592acd48c2db9f578eb008d1bb085b0aac541348
Your Name <you@example.com>
2025-01-16 20:36:50 -0300

    first

---
+++ 1_INTRO/hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    
+      <div className="App">
+        <h1>Hello World React</h1>
+        <div className="teste">
+          <h1>TESTE</h1>
+        </div>
+        
+      </div>
+      
+  )
+}
+
+export default App
