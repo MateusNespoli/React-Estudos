fb8c183a333dbd9c9e6cccf5102538c7edac6246
Your Name <you@example.com>
2025-01-17 20:43:29 -0300

    PT2

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,21 @@
+import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
+
+import Events from './components/Events'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+      <div>
+        <h1>Fundamentos React</h1>
+        <FirstComponent/>
+        <TemplateExpressions/>
+        <Events/>
+      </div>   
+  )
+}
+
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import MyComponent from "./MyComponent";
+
+const FirstComponent = () => {
+    
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+            <MyComponent/>
+        </div>
+
+    )
+}
+
+export default FirstComponent;
