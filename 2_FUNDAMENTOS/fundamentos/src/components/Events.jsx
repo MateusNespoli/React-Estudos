dab1df5fbcc256f75f60aafd895cfaa9d90e40fc
Your Name <you@example.com>
2025-01-20 22:44:29 -0300

    continuação

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -3,11 +3,27 @@ const Events = () => {
         console.log(e);
         console.log("Ativou o evento!");
     };
-    return(
+
+    const renderSomenthing = (x) => {
+        if(x) {
+            return <h1>Renderizando isso!</h1>;
+        } else {
+            return <h1>Também posso renderizar isso!</h1>
+        }
+    }
+
+    return (
        <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou!")}>
+                    Clique aqui também!
+                </button>
+            </div>
+            {renderSomenthing(true)}
+            {renderSomenthing(false)}
         </div>
  
     );
